d7116c4f60646361e1ccde00f5799e35275bce2d
DevAyesh <ayeshmaduranga2001@gmail.com>
2025-12-31 18:01:12 +0530

    add JWT authentication to login user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel.js");
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 
 //@desc Register a user
@@ -43,7 +44,29 @@ const registerUser = asyncHandler(async (req, res) => {
 //@route POST /api/users/login
 //access Public
 const loginUser = asyncHandler(async (req, res) => {
-    res.json({ message: "User logged in successfully" });
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        res.status(400);
+        throw new Error("All fields are mandatory!");
+    }
+    const user = await User.findOne({ email });
+    //compare password with hashed password
+    if (user && (await bcrypt.compare(password, user.password))) {
+        const accessToken = jwt.sign(
+            {
+                user: {
+                    username: user.username,
+                    email: user.email,
+                    id: user.id,
+                },
+            }, process.env.JWT_SECRET,
+            { expiresIn: "1m" });
+        res.status(200).json({ accessToken });
+    }else {
+        res.status(401);
+        throw new Error("Email or password is not valid");
+    }
 });
 
 //@desc Current user info
