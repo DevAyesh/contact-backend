b412fb26a86c7950a322a9e48635b0c4e86d2683
DevAyesh <ayeshmaduranga2001@gmail.com>
2025-12-31 17:38:43 +0530

    add user registration, login

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const port = process.env.PORT || 4001;
 
 app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoutes.js"));
+app.use("/api/users", require("./routes/userRoutes.js"));
 app.use(errorHandler);
 
 app.listen(port, () => {
